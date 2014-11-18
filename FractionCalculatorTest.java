3b5849d34c0c1231ebd3aa253c58421e3fb19713
unknown <stefanm@LONWRK07474.eidos.com>
2014-11-18 14:27:42 +0000

    Replaced inital tests with derived methods from FractionTester.

--- FractionCalculatorTest.java
+++ FractionCalculatorTest.java
@@ -2,16 +2,25 @@ public class FractionCalculatorTest {
 
     public static void main (String[] args) {
         FractionCalculator calc = new FractionCalculator();
-        calc.statePrint();
-
+        
         calc.setState(new Fraction(1, 3));
         calc.setOperator("+");
-        calc.statePrint();
-        
+
         calc.reset();
-        calc.statePrint();
+        test(calc.getState(),new Fraction(1, 3),"error state rest test 1");
+        test(calc.getState(),new Fraction(0, 1),"error state rest test 2");
+        test(calc.getOperator(),"+","error operator reset test 1");
+        test(calc.getOperator(),"","error operator reset test 2");
 
     }
 
+    static void test(Fraction f1, Fraction f2, String msg){
+        if (! f1.equals(f2))
+            System.out.println(msg);
+    }
     
+    static void test(String str1, String str2, String msg){
+        if (! str1.equals(str2))
+            System.out.println(msg);
+    }
 }
